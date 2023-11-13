d54ede49209c0aa9c98b0a2bc294228a7745f80c
giGa-1 <1gledpobgornuk@gmail.com>
2023-11-13 16:11:29 +0100

    initialisation project and making main landing page as well

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils"
+import { ReactNode } from "react"
+
+const MaxWidthWrapper = ({
+    children, 
+    className,
+}: {
+    children: ReactNode,
+    className?:string,
+}) => {
+    return (
+        <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)}>
+            {children}
+        </div>
+    )
+}
+
+export default MaxWidthWrapper
