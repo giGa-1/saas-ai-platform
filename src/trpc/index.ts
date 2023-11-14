99740459258af02b834a9f22612b9e61905d45de
giGa-1 <1gledpobgornuk@gmail.com>
2023-11-14 13:18:02 +0100

    initialized prisma and done with db

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,20 +1,38 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
- 
+import { db } from '@/db';
+
 export const appRouter = router({
- authCallback: publicProcedure.query(async ()=>{
-    const {getUser} = getKindeServerSession();
-    const user = await getUser();
+    authCallback: publicProcedure.query(async () => {
+        const { getUser } = getKindeServerSession();
+        const user = await getUser();
 
-    if(!user||!user.id||!user.email) {
-        throw new TRPCError({code: 'UNAUTHORIZED'});
-    }
+        if (!user || !user.id || !user.email) {
+            throw new TRPCError({ code: 'UNAUTHORIZED' });
+        }
 
-    return {success: true}
- })
+        // if usre in db
+
+        const dbUser =  await db.user.findFirst({
+            where: {
+                id: user.id
+            }
+        })
+
+        if(!dbUser) {
+            await db.user.create({
+                data: {
+                    id: user.id,
+                    email: user.email
+                }
+            })
+        }
+
+        return { success: true }
+    })
 });
- 
+
 // Export type router type signature,
 // NOT the router itself.
 export type AppRouter = typeof appRouter;
