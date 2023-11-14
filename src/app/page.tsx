c1c63a0fefd379e96e18ad04d5bbcece2deef032
giGa-1 <1gledpobgornuk@gmail.com>
2023-11-14 10:45:20 +0100

    made auth and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import {ArrowRight } from 'lucide-react';
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/UI/button";
 import Image from "next/image";
 export default function Home() {
   return (
